8da58ae80d9ed104ad6801ce36da125723bf90c9
andrew <andrewmuhn@gmail.com>
2023-04-30 18:23:36 -0600

    feat: init routes file tree and created userRoutes

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(
   session.Store
 );
+
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
