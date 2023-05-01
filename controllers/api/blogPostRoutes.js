eca12c36cced9d15ed44d649300828a05abbe654
andrew <andrewmuhn@gmail.com>
2023-05-01 09:17:13 -0600

    feat: made blogpost routes

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,3 +1,40 @@
 const router = require('express').Router();
+const { BlogPost } = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
+
+router.post('/', async (req, res) => {
+  try {
+    const newBlogPost = await BlogPost.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlogPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.route('/:id').get(async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findByPk(req.params.id);
+    const blogPost = blogPostData.get({ plain: true });
+    res.status(200).json(blogPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// .update(withAuth, async (req, res) => {
+//   try {
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// })
+// .delete(withAuth, async (req, res) => {
+//   try {
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
